9c5751a98fa876136e3debae5ced4dd804badc77
sergioDominguez <sergiodgdeveloper@gmail.com>
2024-08-13 19:50:26 +0200

    app init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Router from "../src/containers/Router/router.jsx";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Router />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/Router/router.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from "../../views/Home";
+
+const Router = () => {
+  
+
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+
+      </Routes>
+    </BrowserRouter>
+  )
+};
+
+export default Router;
