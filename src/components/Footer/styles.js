b726d0fa1f6d93e3c36d2d1dfa7710ffa5dd27b3
MarioFalcon <maok.seven@gmail.com>
2024-11-14 02:39:04 +0100

    add radio + soundcloud

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -3,19 +3,18 @@ import styled from 'styled-components';
 export const Footer = styled.div`
   background-color: black;
   display: flex;
-  position: fixed;
-  bottom: 0;
+  position: relative;
   width: 100%;
   color: #fff;
   font-size: 0.5vw;
-  justify-content: center; /* Cambiado a 'center' */
- 
-  padding: 10px 10px;
+  justify-content: center;
+  padding: 10px 0; /* Cambiado para evitar desbordamiento horizontal */
   font-family: monospace;
+  margin-top: auto;
+  box-sizing: border-box; /* Asegura que el padding no se agregue al ancho total */
 
   @media (max-width: 880px) {
     font-size: 1.6vw;
     justify-content: center;
-   
   }
 `;
