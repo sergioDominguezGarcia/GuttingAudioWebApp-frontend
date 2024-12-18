c3b7649bf03588ca21b47c52db736eb111005e9c
MarioFalcon <maok.seven@gmail.com>
2024-12-18 07:13:59 +0100

    Fix details artist

--- src/components/ArtistDetail/index.jsx
+++ src/components/ArtistDetail/index.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
+import styled from 'styled-components'
 import { useParams } from 'react-router-dom'
-import * as S from './styles'
-import { artistsData } from './constants' 
+import { artistsData } from './constants'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faInstagram,faSoundcloud } from '@fortawesome/free-brands-svg-icons'
+import { faInstagram, faSoundcloud } from '@fortawesome/free-brands-svg-icons'
 import eps from "../../data/eps.json"
 
-
-const ArtistDetail = () => {
-
-  
+const ArtistDetailview = () => {
   const openLink = (url) => {
     window.open(url, '_blank', 'noopener,noreferrer')
   }
@@ -19,56 +16,175 @@ const ArtistDetail = () => {
   if (!artist) {
     return <p>Artista no encontrado</p>
   }
+
   const filteredEps = eps.filter(
     (ep) => ep.artist.toLowerCase() === artist.name.toLowerCase() // Comparación en minúsculas
   )
 
   return (
-    <S.ArtistDetail>
-      <S.LefttBox>
-        <S.ArtistImage src={artist.image} alt={artist.name} />
-        <S.Releases>          
+    <ArtistDetail>
+      <Header>
+        <h3>{artist.name}</h3>
+        <hr />
+      </Header>
+      <Content>
+        <LeftBox>
+          <ArtistImage src={artist.image} alt={artist.name} />
+          <Releases>
             {filteredEps.map((ep) => (
-              <S.EpCover key={ep.id}>
+              <EpCover key={ep.id}>
                 <img
                   src={ep.coverUrl}
                   alt={`Portada de ${ep.title}`}
-                  style={{ width: '20%', height: 'auto'}}
+                  style={{ width: '20%', height: 'auto' }}
                 />
-              </S.EpCover>
+              </EpCover>
             ))}
-        </S.Releases>
-      </S.LefttBox>
-      <S.RightBox>
-        <S.ArtistName>
-          {' '}
-          <h3>{artist.name}</h3>
-        </S.ArtistName>
-        <S.SocialLinks>
-          <FontAwesomeIcon
-            onClick={() => openLink(artist.socialLinks.instagram)}
-            icon={faInstagram}
-            style={{
-              cursor: 'pointer',
-              height: '4rem',
-            }}
-          />
-          <FontAwesomeIcon
-            onClick={() => openLink(artist.socialLinks.soundcloud)}
-            icon={faSoundcloud}
-            style={{
-              cursor: 'pointer',
-              height: '4rem',
-            }}
-          />
-        </S.SocialLinks>
-        <S.ArtistBio>
-          {' '}
-          <p>{artist.bio}</p>{' '}
-        </S.ArtistBio>
-      </S.RightBox>
-    </S.ArtistDetail>
+          </Releases>
+        </LeftBox>
+        <RightBox>
+          <ArtistBio>
+            <p>{artist.bio}</p>
+          </ArtistBio>
+          <SocialLinks>
+            <FontAwesomeIcon
+              onClick={() => openLink(artist.socialLinks.instagram)}
+              icon={faInstagram}
+              style={{ cursor: 'pointer', height: '2rem' }}
+            />
+            <FontAwesomeIcon
+              onClick={() => openLink(artist.socialLinks.soundcloud)}
+              icon={faSoundcloud}
+              style={{ cursor: 'pointer', height: '2rem' }}
+            />
+          </SocialLinks>
+        </RightBox>
+      </Content>
+    </ArtistDetail>
   )
 }
 
-export default ArtistDetail
+export default ArtistDetailview
+
+const ArtistDetail = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+  padding: 3rem 5rem;
+  max-width: 1920px;
+  @media (max-width: 1200px) {
+    width: 100%;
+    padding: 0rem;
+  }
+  @media (max-width: 425px) {
+    width: 100%;
+    padding: 0rem;
+  }
+`
+
+const Header = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 100%;
+  h3 {
+    font-family: kaneda-gothic-extrabold;
+    font-size: 5.5em;
+    color: rgb(221, 221, 221, 0.95);
+    text-shadow: 2px 1px 2px black;
+    margin: 0;
+    text-transform: uppercase; 
+
+    @media (max-width: 834px) {
+      font-size: 3.5em;
+  }
+  }
+
+
+  hr {
+    width: 100%;
+    border: none;
+    border-bottom: 1px solid gray;
+    margin: 0.5rem 0;
+  }
+`
+
+const Content = styled.div`
+  display: flex;
+  width: 100%;
+  @media (max-width: 1200px) {
+    flex-direction: column;
+  }
+`
+
+const ArtistImage = styled.img`
+  max-width: 55rem;
+  width: 100%;
+  height: auto;
+  display: flex;
+`
+
+const Releases = styled.div`
+  @media (max-width: 1200px) {
+    display: none;
+  }
+`
+
+const EpCover = styled.div`
+  justify-content: center;
+  @media (max-width: 834px) {
+    img {
+      width: 50%;
+    }
+  }
+`
+
+const ArtistBio = styled.div`
+  line-height: 32px;
+  padding: 2rem;
+  color: #dadada;
+  p {
+    font-family: 'kaneda-gothic-light';
+    font-size: 2em;
+    letter-spacing: 1.4px;
+    text-shadow: 1px 1px 2px black;
+    margin: 0;
+  }
+  @media (max-width: 1200px) {
+    padding: 1.5em;
+    p {
+      font-size: 1.8em;
+    }
+  }
+`
+
+const SocialLinks = styled.div`
+  margin: 2em 0;
+  display: flex;
+  gap: 2rem;
+  justify-content: center;
+  @media (max-width: 834px) {
+    margin-left: 3.5em;
+    gap: 1.5rem;
+  }
+`
+
+const LeftBox = styled.div`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  @media (max-width: 1200px) {
+    width: 100%;
+  }
+`
+
+const RightBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 50%;
+  @media (max-width: 1200px) {
+    width: 100%;
+  }
+`
